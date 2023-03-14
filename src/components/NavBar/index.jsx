81c071074e1dde5798572f05df76caa1b082ade1
Guillermo Rocatti <grocatti@hotmail.com>
2023-03-14 17:58:00 -0300

    cambio de logo y favicon

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,7 @@ import CartWidget from "../CartWidget"
 const NavBar = () => {
   return (
     <div className="navbar">
-        <img src="https://img.freepik.com/premium-vector/cocktails-logo-inspiration-drink-glass-bar-restaurant_63578-164.jpg?w=2000" alt="logo" className="logo" />
+        <img src="https://img.freepik.com/vector-gratis/plantilla-logotipo-salon-cocteles-copa-coctel-minima_53876-117632.jpg?size=626&ext=jpg&ga=GA1.2.841188797.1678826306&semt=ais" alt="logo" className="logo" />
         <div className="navText"><a href="#">Home</a></div>
         <div className="navText"><a href="#">Productos</a></div>
         <div className="navText"><a href="#">Contacto</a></div>

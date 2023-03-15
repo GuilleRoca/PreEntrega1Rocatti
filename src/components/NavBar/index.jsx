d1048eb301b38fd0ff0c8e379feeb225a281775a
Guillermo Rocatti <grocatti@hotmail.com>
2023-03-14 21:06:10 -0300

    correccion de logo y estilos

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,7 @@ import CartWidget from "../CartWidget"
 const NavBar = () => {
   return (
     <div className="navbar">
-        <img src="https://img.freepik.com/vector-gratis/plantilla-logotipo-salon-cocteles-copa-coctel-minima_53876-117632.jpg?size=626&ext=jpg&ga=GA1.2.841188797.1678826306&semt=ais" alt="logo" className="logo" />
+        <div><a href="#"><img src="/public/super-logo.png" alt="logo" className="logo" /></a></div>
         <div className="navText"><a href="#">Home</a></div>
         <div className="navText"><a href="#">Productos</a></div>
         <div className="navText"><a href="#">Contacto</a></div>
